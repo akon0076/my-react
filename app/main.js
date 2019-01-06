8ce6da62ddc07ea6d592228e70e90fbf82a7b386
1091262281@qq.com <1091262281@qq.com>
2019-01-06 21:53:58 +0800

    初始化

---
+++ app/main.js
@@ -0,0 +1,10 @@
+// main.js
+import React from 'react';
+import Table from './FlavorForm.jsx';
+import ReactDOM from 'react-dom';
+import './table.css'
+
+ReactDOM.render(
+    <Table />,
+    document.getElementById('root')
+);
